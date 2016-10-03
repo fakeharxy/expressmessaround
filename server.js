366f7fbe7569d0f50d7839d567aedbf39d0a5548
Harxy <harkerp@gmail.com>
2016-10-03 20:10:50 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! Welcome to our api' });
+});
+
+app.use('/api', router);
+
+app.listen(port);
+
