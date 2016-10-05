49a894955a0c4737d36ee37de86196c45de4a780
Harxy <harkerp@gmail.com>
2016-10-05 17:04:35 +0100

    Added mongo integration (needs testing) and tidied

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var app = express();
+var express    = require('express');
+var app        = express();
 var bodyParser = require('body-parser');
-
-var port = process.env.PORT || 8080;
+var port       = process.env.PORT || 8080;
+var mongoose   = require('mongoose');
+var router     = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var router = express.Router();
+mongoose.connect('mongodb://localhost:27017/focusd');
 
 router.get('/', function(req, res) {
   res.json({ message: 'hooray! Welcome to our api' });
 });
 
 app.use('/api', router);
-
 app.listen(port);
